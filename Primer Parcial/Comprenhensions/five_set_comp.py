a37289f6feb40631be988d89e6b93dfcf6d5ce92
Maggy30ev <maggy30ev@gmail.com>
2022-11-04 21:33:21 -0600

    diccionarios

--- Primer Parcial/Comprenhensions/five_set_comp.py
+++ Primer Parcial/Comprenhensions/five_set_comp.py
@@ -13,7 +13,7 @@ def first_set_comp(name_file: str):
         return set_games
 
 
-# Conjunto de todos los registros que sus elementos
+# Conjunto de todos los registros donde sus elementos
 # sean la tupla 'Name' y 'Global_Sales' de los juegos
 def second_set_comp(name_file: str):
     with open(name_file) as csvfile:
@@ -26,7 +26,7 @@ def second_set_comp(name_file: str):
         return set_games
 
 
-# Conjunto de todos los registros que sus elementos
+# Conjunto de todos los registros donde sus elementos
 # sean la tupla 'Name' y 'NA_Sales' de los juegos
 # que hayan vendido más de 5.0 millones en NA
 def third_set_comp(name_file: str):
@@ -42,8 +42,8 @@ def third_set_comp(name_file: str):
 
 
 # Conjunto de tuplas con las columnas: 'Name' y 'Global_Sales'
-# de juegos que su 'Publisher' sea Sega, su 'Platform' 3DS
-# y que sean de Sonic.
+# de juegos donde su 'Publisher' sea Sega, su 'Platform' sea 3DS
+# y que contengan la palabra Sonic.
 def fourth_set_comp(name_file: str):
     with open(name_file) as csvfile:
         games = csv.reader(csvfile, delimiter=",")
@@ -58,7 +58,7 @@ def fourth_set_comp(name_file: str):
 
 
 # Conjunto de los nombres de los 'Publisher' que poseen juegos
-# del genero 'Platform' cuyo lanzamiento está entre los años 2013 y 2016.
+# del genero 'Platform' cuyo lanzamiento esté entre los años 2013 y 2016.
 def five_set_comp(name_file: str):
     with open(name_file) as csvfile:
         games = csv.reader(csvfile, delimiter=",")
@@ -75,7 +75,7 @@ def five_set_comp(name_file: str):
 
 
 # Conjunto de tuplas con las columnas: Name y Global_Sales
-# de juegos que su plublisher sea Sega, su plataforma 3DS.
+# de juegos que su plublisher sea Sega y su plataforma 3DS.
 def conjunto_p(name_file: str):
     with open(name_file) as csvfile:
         games = csv.reader(csvfile, delimiter=",")
@@ -133,19 +133,19 @@ def run():
     pprint(set(list(second_set_comp(f))[:5]))
 
     print(
-        "\n3. Conjunto de todos los registros que sus elementos sean la tupla "
+        "\n3. Conjunto de todos los registros donde sus elementos sean la tupla "
         "'Name' y 'NA_Sales' de los juegos que hayan vendido más de 5.0 "
         "millones en NA")
     pprint(set(list(third_set_comp(f))[:5]))
 
     print(
         "\n4. Conjunto de tuplas con las columnas: 'Name' y 'Global_Sales' de juegos "
-        "que su 'Publisher' sea Sega, su 'Platform' 3DS y que sean de Sonic")
+        "donde su 'Publisher' sea Sega, su 'Platform' sea 3DS y que contengan la palabra Sonic")
     pprint(set(list(fourth_set_comp(f))[:5]))
 
     print(
         "\n5. Conjunto de los nombres de los 'Publisher' que poseen juegos del "
-        "genero 'Platform' cuyo lanzamiento está entre los años 2013 y "
+        "genero 'Platform' cuyo lanzamiento esté entre los años 2013 y "
         "2016")
     pprint(set(list(five_set_comp(f))[:5]))
 
@@ -153,46 +153,40 @@ def run():
 
     print("\n\n--------------- Operaciones con conjuntos:\n")
 
-    # 1. Obtener el conjuntos de las tuplas donde los juegos su 'Platform' sea 3DS, su 'Publisher' sea Sega
-    # y que su nombre no contenga la palabra Sonic.
+    # 1. Obtener el conjunto de las tuplas de los juegos donde su 'Platform' sea 3DS, su 'Publisher' sea Sega
+    # y su nombre no contenga la palabra Sonic.
     A = conjunto_p(f) - fourth_set_comp(f)
     print((
-        "\n1. Conjunto de las tuplas donde los juegos 'Platform' sea 3DS,"
-        " su 'Publisher' sea Sega y que su nombre no contenga la palabra Sonic"))
+        "\n1. Conjunto de las tuplas de los juegos donde su 'Platform' sea 3DS, su 'Publisher' sea Sega y su nombre no contenga la palabra Sonic."))
     pprint(set(list(A)[:5]))
 
     # 2. Obtener el conjunto de los 'Publisher' que poseen juegos del genero 'Platform' cuyo
-    # lanzamiento esta entre los años 2013 y 2016 y los nombres de las plataformas.
+    # lanzamiento esta entre los años 2013-2016 y los nombres de las plataformas.
     B = five_set_comp(f).union(first_set_comp(f))
     print((
-        "\n2. Conjunto de los 'Publisher' que poseen juegos del genero 'Platform' "
-        "cuyo lanzamiento esta entre los años 2013 y 2016 y los nombres de las plataformas"
+        "\n2. Conjunto de los 'Publisher' que poseen juegos del genero 'Platform' cuyo lanzamiento esta entre los años 2013-2016 y los nombres de las plataformas."
     ))
     pprint(set(list(B)[:5]))
 
-    # 3. Obtener el conjunto de tuplas que pertenezcan tanto al conjunto A y el resultado de la llamada
-    # a la función second_set_comp(f)
+    # 3. Obtener el conjunto de tuplas que pertenezcan tanto al conjunto A y el resultado
+    # de ejercicio 2 de la sección anterior.
     C = A.intersection(second_set_comp(f))
     print(
-        ("\n3. Conjunto de tuplas que pertenezcan tanto al conjunto A y el "
-         "resultado de la llamada a la función second_set_comp(f)"))
+        ("\n3. Conjunto de tuplas que pertenezcan tanto al conjunto A y el resultado de ejercicio 2 de la sección anterior."))
     pprint(set(list(C)[:5]))
 
-    # 4. Obtener el conjunto complemento de las plataformas que pertenecen a las llamadas de las funciones
-    # first_set_comp(f) y conjunto_q(f)
+    # 4. Obtener el conjunto complemento de las plataformas que pertenecen a los resultados
+    # del ejercicio 1 de la sección anterior y el conjunto de la llamada a la función 
+    # conjunto_q(f).
     D = conjunto_q(f).symmetric_difference(first_set_comp(f))
-    print(("\n4. Conjunto complemento de las plataformas que pertenecen a las "
-           "llamadas de las funciones first_set_comp(f) y conjunto_q(f)"))
+    print(("\n4. Conjunto complemento de las plataformas que pertenecen a los resultados del ejercicio 1 de la sección anterior y el conjunto de la llamada a la función conjunto_q(f)."))
     pprint(set(list(D)[:5]))
 
     # 5. Obtener el conjunto de tuplas donde las ventas de los juegos que se encuentren en el conjunto
-    # de la llamada a la función third_set_comp(f) y en el conjunto de la llamda a la función
-    # conjunto_u(f)
+    # del ejercicio 3 de la sección anterior y en el conjunto de la llamda a la función
+    # conjunto_u(f).
     E = third_set_comp(f) & conjunto_u(f)
-    print((
-        "\n5. Conjunto de tuplas donde las ventas de los juegos que se "
-        "encuentren en el conjunto de la llamada a la función third_set_comp(f) "
-        "y en el conjunto de la llamda a la función conjunto_u(f)"))
+    print(("\n5. Conjunto conjunto de tuplas donde las ventas de los juegos que se encuentren en el conjunto del ejercicio 3 de la sección anterior y en el conjunto de la llamda a la función conjunto_u(f)."))
     pprint(set(list(E)[:5]))
 
 
